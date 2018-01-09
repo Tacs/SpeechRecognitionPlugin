8e19fc69c7f768291525c2bf6a3c7edbc25e9bf6
Tacs <2198169+Tacs@users.noreply.github.com>
2018-01-09 11:44:09 +0000

    Update SpeechRecognition.java

--- src/android/SpeechRecognition.java
+++ src/android/SpeechRecognition.java
@@ -126,6 +126,8 @@ public class SpeechRecognition extends CordovaPlugin {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,lang);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS,5000);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS,10000);
 
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
 
